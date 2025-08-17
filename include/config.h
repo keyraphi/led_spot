281f52e4de66ecf62265cccca49a7dc48a9a58ad
Raphael Braun <keyraphi@gmail.com>
2025-08-17 13:26:06 +0200

    Config file for secret ssid and password (wifi)

---
+++ include/config.h
@@ -0,0 +1,16 @@
+/**
+ * @file config.h
+ * @brief Configuration file for WiFi credentials.
+ *
+ * NOTE: It is highly recommended to add this file to your
+ * .gitignore to avoid committing your WiFi password to a public repository.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi Credentials
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+
+#endif
